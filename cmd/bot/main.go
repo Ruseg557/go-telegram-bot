d31edac147f0a57bce3791e84adfa88f2cfed843
Ruseg557 <abcsamprp@gmail.com>
2026-03-11 00:11:42 +0300

    feat: добавлено создание бота, метод UserName

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -2,21 +2,26 @@ package main
 
 import (
 	"github.com/Ruseg557/go-telegram-bot/internal/config"
+	"github.com/Ruseg557/go-telegram-bot/internal/services/telegram"
 	"log"
 )
 
 func main() {
-	// Загружаем конфигурациюп
+	// Загружаем конфигурацию
 	cfg, err := config.Load()
 	if err != nil {
 		log.Fatal("Ошибка загрузки конфигурации:", err)
 	}
 
-	log.Println("Конфигурация успешно загружена!")
-	log.Printf("Токен бота: %s...", cfg.BotToken[:10])
-	log.Printf("Путь к модели: %s", cfg.ModelPath)
-	log.Printf("Режим отладки: %v", cfg.DebugMode)
+	// Создаем бота
+	bot, err := telegram.NewBot(cfg)
+	if err != nil {
+		log.Fatal("Ошибка создания бота:", err)
+	}
 
-	// TODO: здесь будет инициализация бота
-	log.Println("Бот готов к запуску...")
+	// Проверка
+	log.Printf("Бот авторизован как %s", bot.UserName())
+
+	// TODO: обработка сообщений
+	log.Println("Бот готов к работе!")
 }
--- internal/services/telegram/client.go
+++ internal/services/telegram/client.go
@@ -1,9 +1,8 @@
-package client
+package telegram
 
 import (
 	"github.com/Ruseg557/go-telegram-bot/internal/config"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
-	"log"
 )
 
 type Bot struct {
@@ -14,7 +13,11 @@ type Bot struct {
 func NewBot(cfg *config.Config) (*Bot, error) {
 	bot, err := tgbotapi.NewBotAPI(cfg.BotToken)
 	if err != nil {
-		log.Panic(err)
+		return nil, err
 	}
 	return &Bot{api: bot, config: cfg}, nil
+}
+
+func (b Bot) UserName() string {
+	return b.api.Self.UserName
 }
